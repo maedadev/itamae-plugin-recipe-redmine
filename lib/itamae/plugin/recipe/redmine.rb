fd72df8f89bf17de5bc0fecf4d866a7c8b601e47
y-matsuda <matsuda@lab.acs-jp.com>
2019-05-22 10:28:00 +0900

    initial commit

---
+++ lib/itamae/plugin/recipe/redmine.rb
@@ -0,0 +1,12 @@
+require "itamae/plugin/recipe/redmine/version"
+
+module Itamae
+  module Plugin
+    module Recipe
+      module Redmine
+        class Error < StandardError; end
+        # Your code goes here...
+      end
+    end
+  end
+end
