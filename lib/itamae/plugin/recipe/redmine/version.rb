c2f949f253bd1f478cf0bbcdbd283d32a6b4ba7b
y-matsuda <matsuda@lab.acs-jp.com>
2019-06-27 20:42:38 +0900

    bump up version

--- lib/itamae/plugin/recipe/redmine/version.rb
+++ lib/itamae/plugin/recipe/redmine/version.rb
@@ -2,7 +2,7 @@ module Itamae
   module Plugin
     module Recipe
       module Redmine
-        VERSION = "0.0.1"
+        VERSION = "0.0.2"
 
         REDMINE_VERSION = [
           REDMINE_VERSION_MAJOR = '3',
