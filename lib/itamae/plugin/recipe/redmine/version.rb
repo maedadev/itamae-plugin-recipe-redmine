933e27366e3bd9944069f4bc9e4620431e1140be
y-sugawara-acs <y-sugawara@lab.acs-jp.com>
2020-04-28 16:34:35 +0900

    bump up version

--- lib/itamae/plugin/recipe/redmine/version.rb
+++ lib/itamae/plugin/recipe/redmine/version.rb
@@ -2,7 +2,7 @@ module Itamae
   module Plugin
     module Recipe
       module Redmine
-        VERSION = "0.1.1"
+        VERSION = "0.1.2"
 
         REDMINE_VERSION = [
           REDMINE_VERSION_MAJOR = '4',
