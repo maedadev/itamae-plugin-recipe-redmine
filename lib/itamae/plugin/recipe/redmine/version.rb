ccf16fa0960ee080a1d3485d371531354e88f5e2
ido <f-ido@lab.acs-jp.com>
2022-07-13 06:40:42 +0900

    add 'secure: true' to 'config.session_store :cookie_store'

--- lib/itamae/plugin/recipe/redmine/version.rb
+++ lib/itamae/plugin/recipe/redmine/version.rb
@@ -2,7 +2,7 @@ module Itamae
   module Plugin
     module Recipe
       module Redmine
-        VERSION = "0.1.5"
+        VERSION = "0.1.6"
 
         REDMINE_VERSION = [
           REDMINE_VERSION_MAJOR = '4',
